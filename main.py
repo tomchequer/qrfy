a6ef31374d768dd51b91d600f531ba78eb26d25e
antonio <antoniochequer@gmail.com>
2021-11-17 22:39:40 -0300

    finished the core of the app

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask_cors import CORS
 
 import qrcode
 
-from io import BytesIO
+from io import BytesIO, StringIO
+
+from werkzeug.wrappers import response
 
 app = Flask(__name__)
 CORS(app)
@@ -19,15 +21,19 @@ def not_found(error):
     return '<p>something went wrong!<p>', 404
 
 @app.route('/', methods=['POST', 'GET'])
-def generate_qr_code(text):
+def generate_qr_code():   
     if request.method == 'POST':
-        request.url['the_url']
-        return 'the_url'
-    qr_code_img = qrcode.make(text)
-    qr_code_img.save()
-
-
-
+        buffer = BytesIO()
+        the_url = request.form['the_url']
+        
+        img = qrcode.make(the_url)
+        img.save(buffer)
+        buffer.seek(0)
+        
+        response = send_file(buffer, mimetype='image/png')
+        
+        return response
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
